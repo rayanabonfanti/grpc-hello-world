11055e16dae07f4f8a25b4fbb85fea7ce7ec4daa
rayanabonfanti <rayanabonfanti@gmail.com>
2024-07-03 20:08:06 -0300

    feat: initial commit

--- src/main/java/grpc/hello/world/GreeterServiceImpl.java
+++ src/main/java/grpc/hello/world/GreeterServiceImpl.java
@@ -4,36 +4,16 @@ import grpc.hello.world.models.GreeterGrpc;
 import grpc.hello.world.models.HelloReply;
 import grpc.hello.world.models.HelloRequest;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
-import java.util.Random;
-import java.util.stream.IntStream;
-
+@GrpcService
 public class GreeterServiceImpl extends GreeterGrpc.GreeterImplBase {
 
-    private static final String[] names = {
-            "Alice", "Bob", "Carol", "Dave", "Eve",
-            "Frank", "Grace", "Hank", "Irene", "John",
-            "Kyle", "Laura", "Mike", "Nora", "Oscar",
-            "Patty", "Quinn", "Rachel", "Steve", "Tina",
-            "Uma", "Victor", "Wendy", "Xavier", "Yolanda",
-            "Zack"
-    };
-
-    private static final Random random = new Random();
-
     @Override
-    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) throws InterruptedException {
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
         HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
-        IntStream.range(0, 1000)
-                .mapToObj(i -> (i + 1) + " - " + getRandomName())
-                .forEach(System.out::println);
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
-    }
-
-    private static String getRandomName() {
-        int index = random.nextInt(names.length);
-        return names[index];
     }
 
 }
--- src/main/java/grpc/hello/world/Main.java
+++ src/main/java/grpc/hello/world/Main.java
@@ -1,19 +1,13 @@
 package grpc.hello.world;
 
-import io.grpc.ServerBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        var serverBuilder = ServerBuilder.forPort(6565)
-                .addService(new GreeterServiceImpl())
-                .build();
-
-        serverBuilder.start();
-
-        System.out.println("Application started with gRPC port: " + serverBuilder.getPort());
-
-        serverBuilder.awaitTermination();
+        SpringApplication.run(Main.class, args);
     }
 
 }
